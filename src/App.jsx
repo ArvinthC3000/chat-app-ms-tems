da84b48fa01978d55f40fed84f8137ad6ad09ac2
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-12 20:22:43 +0530

    Materialize init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+import M from 'materialize-css/dist/js/materialize.min.js';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/about/About';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+
   return (
     <>
       <Router>
