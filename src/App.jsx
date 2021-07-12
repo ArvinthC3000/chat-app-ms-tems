fdc6271d2af7ccfc6d4cd0cca786de8fec82f32c
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-12 20:28:39 +0530

    Restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
-import Home from './components/Home';
+import Home from './components/home/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/about/About';
 import { useEffect } from 'react';
