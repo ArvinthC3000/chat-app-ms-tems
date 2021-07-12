59a9a18d87f81754ed60496127d59b775527555f
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-12 21:22:48 +0530

    get default

--- src/actions/movieActions.jsx
+++ src/actions/movieActions.jsx
@@ -14,22 +14,17 @@ const api_key = `04c35731a5ee918f014970082a0088b1`; // TMDB
 const tmdb_url = `https://api.themoviedb.org/3`; // TMDB
 
 // Get Default movies
-export const getDefaultMovies = () => {
-  console.log('here');
-  return async dispatch => {
-    console.log('also here');
-    try {
-      const response = await axios.get(`${tmdb_url}/movie/popular`, {
-        params: { api_key, language: 'en-US', page: '1' },
-      });
-      const { results } = response.data;
-      console.log(results);
-      dispatch({
-        type: SET_DEFAULT_MOVIES,
-        payload: results,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+export const getDefaultMovies = () => async dispatch => {
+  try {
+    const response = await axios.get(`${tmdb_url}/movie/popular`, {
+      params: { api_key, language: 'en-US', page: '1' },
+    });
+    const { results } = response.data;
+    dispatch({
+      type: SET_DEFAULT_MOVIES,
+      payload: results,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
--- src/components/home/Movies.jsx
+++ src/components/home/Movies.jsx
@@ -1,20 +1,42 @@
-const Movies = () => {
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getDefaultMovies } from '../../actions/movieActions';
+import PropTypes from 'prop-types';
+import MoviesList from './MoviesList';
+
+const Movies = ({ getDefaultMovies, movie }) => {
+  const { movies, popular, searchString } = movie;
+
+  useEffect(() => {
+    getDefaultMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <div className='header'>
-        {/* {movies !== null && !movies.length ? 'Popular' : `Search result`} */}
+        {movies === null ? 'Popular' : `Search result`}
       </div>
       <div className='movieContainer'>
-        {/* {movies == null ? (
-              popular.map(movie => <MoviesList key={movie.id} movie={movie} />)
-            ) : !movies.length ? (
-              <div className='header'>No result found for '{searchString}'</div>
-            ) : (
-              movies.map(movie => <MoviesList key={movie.id} movie={movie} />)
-            )} */}
+        {movies == null ? (
+          popular &&
+          popular.map(movie => <MoviesList key={movie.id} movie={movie} />)
+        ) : !movies.length ? (
+          <div className='header'>No result found for '{searchString}'</div>
+        ) : (
+          movies.map(movie => <MoviesList key={movie.id} movie={movie} />)
+        )}
       </div>
     </>
   );
 };
 
-export default Movies;
+const mapStatetoProps = state => ({
+  movie: state.movie,
+});
+
+Movies.propTypes = {
+  getDefaultMovies: PropTypes.func.isRequired,
+};
+
+export default connect(mapStatetoProps, { getDefaultMovies })(Movies);
